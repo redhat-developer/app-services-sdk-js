b42b6dbba92c8eec1359ce6583251a72fe57ae51
Wojciech Trocki <wtrocki@redhat.com>
2021-05-27 12:19:21 +0100

    fix: adding examples

---
+++ examples/src/registry.ts
@@ -0,0 +1,18 @@
+import { Configuration, DefaultApi } from "../../packages/registry-management-sdk/src";
+
+const accessToken = process.env.CLOUD_API_TOKEN;
+const basePath = "https://api.openshift.com";
+
+const apiConfig = new Configuration({
+    accessToken,
+    basePath
+})
+
+const registryApi = new DefaultApi(apiConfig)
+
+registryApi.listRegistries().then((data) => {
+    console.log(data?.data)
+}).catch((err) => {
+    console.error(err.message)
+})
+
