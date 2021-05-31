6faa6281e0fa7508c1c82e7a6a91df3a61afba92
Wojciech Trocki <wtrocki@redhat.com>
2021-05-31 23:06:34 +0100

    wip: homebrewn release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+GITHUB_REF_SLUG=kafka-management@v0.0.1
 echo "Release process for TAG: $GITHUB_REF_SLUG"
 
+## Pick right release based on tag structure
+dirname $0`/lib/releaseFromTag $GITHUB_REF_SLUG`
+
+ 
