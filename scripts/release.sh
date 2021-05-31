1f80a6271ce6d5611eaefe15d869635d8426adc6
Wojciech Trocki <wtrocki@redhat.com>
2021-05-31 23:15:03 +0100

    fix: enable release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,9 @@
 GITHUB_REF_SLUG=kafka-management@v0.0.1
 echo "Release process for TAG: $GITHUB_REF_SLUG"
 
+SCRIPT_DIR=`dirname $0`
 ## Pick right release based on tag structure
-dirname $0`/lib/releaseFromTag $GITHUB_REF_SLUG`
+EXEC=`$SCRIPT_DIR/lib/releaseFromTag $GITHUB_REF_SLUG`
 
+$EXEC 
  
