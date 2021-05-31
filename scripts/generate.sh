499bad4de802a1a749ac374aa7fbb6028ef5e017
Wojciech Trocki <wtrocki@redhat.com>
2021-05-31 09:32:32 +0100

    chore: cleanup other SDK's when generating code

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -24,23 +24,27 @@ OUTPUT_PATH="packages/registry-management-sdk/src/generated"
 
 echo "Generating based on ${OPENAPI_FILENAME}"
 
+rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
+
 npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
     "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
     --package-name="${PACKAGE_NAME}" \
     --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
     --ignore-file-override=.openapi-generator-ignore
 
-# OPENAPI_FILENAME=".openapi/kafka-admin.yml"
-# PACKAGE_NAME="@rhoas/kafka-admin-sdk"
-# OUTPUT_PATH="packages/kafka-admin-sdk/src/generated"
+OPENAPI_FILENAME=".openapi/kafka-admin.yml"
+PACKAGE_NAME="@rhoas/kafka-admin-sdk"
+OUTPUT_PATH="packages/kafka-admin-sdk/src/generated"
 
-# echo "Generating based on ${OPENAPI_FILENAME}"
+rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
 
-# npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
-#     "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
-#     --package-name="${PACKAGE_NAME}" \
-#     --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
-#     --ignore-file-override=.openapi-generator-ignore
+echo "Generating based on ${OPENAPI_FILENAME}"
+
+npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
+    "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
+    --package-name="${PACKAGE_NAME}" \
+    --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
+    --ignore-file-override=.openapi-generator-ignore
 
 
 
