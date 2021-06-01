5f550cb2e4572ec9ceabf823fcd3fee5f92a04c2
Wojciech Trocki <wtrocki@redhat.com>
2021-06-01 13:06:07 +0100

    chore: change openapi update automation
    use new payload key and generate code in directly in PR

--- scripts/fetch_api.sh
+++ scripts/fetch_api.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # get the download URL of the OpenAPI spec file
-OPENAPI_FILE_URL=`echo $CLIENT_PAYLOAD | jq .download`
+OPENAPI_FILE_URL=`echo $CLIENT_PAYLOAD | jq .download_url`
 
 # download the OpenAPI file
 curl -H "Authorization: token $BF2_TOKEN" "$OPENAPI_FILE_URL"
