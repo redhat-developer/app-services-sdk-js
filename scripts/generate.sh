0b28b5a15c2d6586c995b5933fb0c9801e22865b
Enda <ephelan@redhat.com>
2021-06-23 09:40:53 +0100

    chore: refactor generate script and add connectors_mgmt (#68)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,51 +1,48 @@
 #!/usr/bin/env bash
 ## OPENAPI_FILENAME=yourapi generate_js.sh 
 
+# generate an API client for a service
+generate_sdk() {
+    local file_name=$1
+    local output_path=$2
+    local package_name=$3
+
+    echo "Generating based on ${file_name}"
+
+    # remove old generated models
+    rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
+
+    npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
+    "$file_name" -o "$output_path" \
+    --package-name="${package_name}" \
+    --additional-properties=$additional_properties \
+    --ignore-file-override=.openapi-generator-ignore
+}
+
 npx @openapitools/openapi-generator-cli version-manager set 5.1.1
 echo "Generating SDKs"
+additional_properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api"
 
 OPENAPI_FILENAME=".openapi/kas-fleet-manager.yaml"
 PACKAGE_NAME="@rhoas/kafka-management-sdk"
 OUTPUT_PATH="packages/kafka-management-sdk/src/generated"
 
-echo "Generating based on ${OPENAPI_FILENAME}"
-
-rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
-
-npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
-    "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
-    --package-name="${PACKAGE_NAME}" \
-    --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
-    --ignore-file-override=.openapi-generator-ignore
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
 OPENAPI_FILENAME=".openapi/srs-fleet-manager.json"
 PACKAGE_NAME="@rhoas/registry-management-sdk"
 OUTPUT_PATH="packages/registry-management-sdk/src/generated"
 
-echo "Generating based on ${OPENAPI_FILENAME}"
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
-rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
+OPENAPI_FILENAME=".openapi/connector_mgmt.yaml"
+PACKAGE_NAME="@rhoas/connector-management-sdk"
+OUTPUT_PATH="packages/connector-management-sdk/src/generated"
 
-npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
-    "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
-    --package-name="${PACKAGE_NAME}" \
-    --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
-    --ignore-file-override=.openapi-generator-ignore
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
 
 OPENAPI_FILENAME=".openapi/kafka-admin-rest.yaml"
 PACKAGE_NAME="@rhoas/kafka-instance-sdk"
 OUTPUT_PATH="packages/kafka-instance-sdk/src/generated"
 
-rm -Rf $OUTPUT_PATH/model $OUTPUT_PATH/api
-
-echo "Generating based on ${OPENAPI_FILENAME}"
-
-npx @openapitools/openapi-generator-cli generate -g typescript-axios -i \
-    "$OPENAPI_FILENAME" -o "$OUTPUT_PATH" \
-    --package-name="${PACKAGE_NAME}" \
-    --additional-properties="ngVersion=6.1.7,npmName=${PACKAGE_NAME},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api " \
-    --ignore-file-override=.openapi-generator-ignore
-
-
-
-
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
