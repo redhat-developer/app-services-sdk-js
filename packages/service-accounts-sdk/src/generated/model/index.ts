f8072c282ce822b4109dc6487c60c5ed7912bb07
Red Hat Application Services CI <84090353+app-services-ci@users.noreply.github.com>
2022-07-01 11:44:18 +0100

    chore(all): re-generate OpenAPI client(s) (#453)
    Co-authored-by: app-services-ci <app-services-ci@users.noreply.github.com>

--- packages/service-accounts-sdk/src/generated/model/index.ts
+++ packages/service-accounts-sdk/src/generated/model/index.ts
@@ -1,4 +1,6 @@
 export * from './model-error';
+export * from './red-hat-error-representation';
 export * from './service-account-create-request-data';
 export * from './service-account-data';
 export * from './service-account-request-data';
+export * from './validation-exception-data';
