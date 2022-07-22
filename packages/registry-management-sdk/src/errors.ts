17c688c8f15ab5aae8b72c9080b266d91f09524a
app-services-ci <app-services-ci@users.noreply.github.com>
2022-07-22 11:06:52 +0000

    fix(smartevents-mgmt/v1): update for smartevents-mgmt/v1 SDK

--- packages/registry-management-sdk/src/errors.ts
+++ packages/registry-management-sdk/src/errors.ts
@@ -43,4 +43,16 @@ export const APIErrorCodes = {
   /** Bad request format - unsupported media type*/
   ERROR_10 : "SRS-MGMT-10", 
 
+  /** Could not check quota for user?*/
+  ERROR_11 : "SRS-MGMT-11", 
+
+  /** Evaluation instances not allowed.*/
+  ERROR_12 : "SRS-MGMT-12", 
+
+  /** User already has the maximum number of allowed Evaluation instances.*/
+  ERROR_13 : "SRS-MGMT-13", 
+
+  /** Total (global) number of instances exhausted.*/
+  ERROR_14 : "SRS-MGMT-14", 
+
 }
